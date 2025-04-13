7bca21ce1ab9e93eb4cb06816be091e3355d652f
Hu6r1s <darmaa333@gmail.com>
2025-04-13 19:00:36 +0900

    script(env): add env variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import './App.css';
 import { LoginPage } from "@pages/LoginPage";
+import './App.css';
 
 function App() {
   return (
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const LoginPage: React.FC = () => {
   const handleGoogleLogin = () => {
-    window.location.href = "";
+    window.location.href = process.env.REACT_APP_API_URL as string;
   };
 
   return (
