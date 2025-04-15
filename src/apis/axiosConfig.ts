76f266014b926d52a32b0b474b0508e5dd38a8c9
hu6r1s <darmaa333@gmail.com>
2025-04-15 17:23:52 +0900

    fix(Auth): Unauthorized error

--- src/apis/axiosConfig.ts
+++ src/apis/axiosConfig.ts
@@ -5,4 +5,21 @@ const apiClient: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // 401 에러 처리
+      console.error("Unauthorized! Redirecting to login...");
+      alert('인증 정보가 유효하지 않습니다. 다시 로그인해주세요.');
+
+      window.location.href = '/login'; // '/login'은 실제 로그인 페이지 경로
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
